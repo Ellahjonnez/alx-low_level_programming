e830c028bd1510a2137b9e6e3557c5d0ff6a24d8
Ellahjonnez <ellahjonnez@gmail.com>
2023-02-16 09:29:10 -0800

    A C program that prints exactly a line using the puts functon

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a line using the function puts
+ * Return: 0
+ */
+int main(void)
+{
+puts(""Programming is like building a multilingual puzzle\n");
+return (0);
+}
