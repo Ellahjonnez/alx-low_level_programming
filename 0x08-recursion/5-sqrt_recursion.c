10072ee74e1f0775b68d2bccbc2ffcff514aed24
Ellahjonnez <ellahjonnez@gmail.com>
2023-03-09 08:51:26 -0800

    Task5: A function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * mainsqrt - To find the natural square root of a number
+ * @n: The value for square root calculation
+ * @i: iteration value
+ * Return: the resulting square root
+ */
+
+int mainsqrt(int n, int i)
+{
+		if (i * i > n)
+		{
+			return (-1);
+		}
+		if (i * i == n)
+		{
+			return (i);
+		}
+		return (mainsqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - To return the natural square root of a number.
+ * @n: The natural number to calculate the square root
+ * Return: The natural root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+        if (n < 0)
+        {
+                return (-1);
+        }
+        if (n == 1)
+        {
+                return (1);
+        }
+        else
+        {
+                return (mainsqrt(n, 0));
+        }
+}
+
