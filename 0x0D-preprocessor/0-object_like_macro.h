b0b03f93f33d9b54f56926c4f78f212a0c2fc823
Ellahjonnez <ellahjonnez@gmail.com>
2023-03-20 23:36:46 -0700

    Task0: Object-like Macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 /**
- * File name: Object_like_macro.h
- * Description: A header file that defines a macro named SIZE
- * as an abbreviation for the token 1024.
+ * File name: 0-object_like_macro.h
+ * Description: A header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024.
  */
 
 #define SIZE 1024
