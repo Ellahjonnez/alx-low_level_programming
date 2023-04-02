6b7c97c45445e1b8d6f9dc2e723bd0eefcc14c8d
Ellahjonnez <Ellahjonnez>
2023-04-02 12:46:52 -0700

    a function that prints a sentence

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myfirst - to print a sentece before main
+ */
+
+void myfirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
